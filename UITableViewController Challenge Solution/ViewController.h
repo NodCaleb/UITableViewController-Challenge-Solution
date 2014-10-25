5ed84399afcce869321a0a54042ba370001d3a16
Eugene Rozhkov <eugenerozhkov@Teiwaz.local>
2014-10-25 17:42:47 +0300

    Initial Commit

---
+++ UITableViewController Challenge Solution/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  UITableViewController Challenge Solution
+//
+//  Created by Eugene Rozhkov on 25.10.14.
+//  Copyright (c) 2014 nordPoint. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ UITableViewController Challenge Solution/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  UITableViewController Challenge Solution
+//
+//  Created by Eugene Rozhkov on 25.10.14.
+//  Copyright (c) 2014 nordPoint. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
